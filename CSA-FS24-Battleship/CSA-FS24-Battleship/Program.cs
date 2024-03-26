5c80422284064e0bc33de8aa108530f8a72c5687
davidbohner <davidbohner@protonmail.ch>
2024-03-26 14:10:27 +0100

    init

---
+++ CSA-FS24-Battleship/CSA-FS24-Battleship/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace CSA_FS24_Battleship;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ CSA-FS24-Battleship/Explorer700Library/Joystick/Joystick.cs
@@ -0,0 +1,71 @@
+﻿// ----------------------------------------------------------------------------
+// CSA - C# in Action
+// (c) 2024, Christian Jost, HSLU
+// ----------------------------------------------------------------------------
+using System;
+using System.Device.Gpio;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Explorer700Library
+{
+    public class Joystick
+    {
+        #region members & events
+        private int centerPin;
+        public event EventHandler<KeyEventArgs> JoystickChanged;
+        #endregion
+
+        #region constructor & destructor
+        public Joystick(Pcf8574 pcf8574, GpioController gpioController)
+        {
+            Pcf8574 = pcf8574;
+            pcf8574.Mask |= 0x0F;
+            centerPin = 20;
+            GpioController = gpioController;
+            GpioController.OpenPin(centerPin, PinMode.InputPullUp);
+
+            // Start Polling-Thread
+            Thread t = new Thread(Run);
+            t.IsBackground = true;
+            t.Start();
+        }
+        #endregion
+
+        #region properties
+        private Pcf8574 Pcf8574 { get; set; }
+
+        internal GpioController GpioController { get; }
+
+        /// <summary>
+        /// Liest und liefert den Zustand des Joysticks
+        /// </summary>
+        public Keys Keys
+        {
+            get
+            {
+                Keys k = Keys.NoKey;
+                // ToDo
+                if (!(bool)GpioController.Read(centerPin)) k |= Keys.Center;
+                return k;
+            }
+        }
+        #endregion
+
+        #region methods
+        /// <summary>
+        /// Pollt alle 50ms den Joystick und generiert ein JoystickChanged Event, falls
+        /// sich der Zustand des Joysticks (Taste gedrückt/losgelassen) verändert hat.
+        /// </summary>
+        private void Run()
+        {
+            Keys oldState = Keys;
+            while (true)
+            {
+                // ToDo
+                Thread.Sleep(50);
+            }
+        }
+        #endregion
+    }
+}
