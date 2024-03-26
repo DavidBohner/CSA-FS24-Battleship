77989ef9eee8ce46af6702dd48cae04a7dda282f
davidbohner <davidbohner@protonmail.ch>
2024-03-26 14:26:20 +0100

    added proj reference

--- CSA-FS24-Battleship/CSA-FS24-Battleship/Program.cs
+++ CSA-FS24-Battleship/CSA-FS24-Battleship/Program.cs
@@ -1,7 +1,9 @@
-﻿namespace CSA_FS24_Battleship;
+﻿using Explorer700Library;
 
+namespace CSA_FS24_Battleship;
 class Program
 {
+    private static Explorer700 _explorer700 = new();
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
--- CSA-FS24-Battleship/CSA-FS24-Battleship/obj/Debug/net8.0/CSA-FS24-Battleship.AssemblyInfo.cs
+++ CSA-FS24-Battleship/CSA-FS24-Battleship/obj/Debug/net8.0/CSA-FS24-Battleship.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSA-FS24-Battleship")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce82f2215bf245cd1f9456ec0542c4c778fbf235")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c80422284064e0bc33de8aa108530f8a72c5687")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSA-FS24-Battleship")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSA-FS24-Battleship")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
