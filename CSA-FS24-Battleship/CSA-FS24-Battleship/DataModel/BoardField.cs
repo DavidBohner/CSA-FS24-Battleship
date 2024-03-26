db6c0585bd8ab2e38e21b14547e90a4a1201a642
Matthias Brunner <brunner.matthias@outlook.com>
2024-03-26 17:10:19 +0100

    feat:add gameboard fields

---
+++ CSA-FS24-Battleship/CSA-FS24-Battleship/DataModel/BoardField.cs
@@ -0,0 +1,6 @@
+namespace CSA_FS24_Battleship.DataModel;
+
+public class BoardField
+{
+    public bool WasShot { get; set; } = false;
+}
--- CSA-FS24-Battleship/CSA-FS24-Battleship/DataModel/GameBoard.cs
+++ CSA-FS24-Battleship/CSA-FS24-Battleship/DataModel/GameBoard.cs
@@ -5,18 +5,35 @@ public class GameBoard
 {
     public readonly int Size;
     public readonly List<Ship> Ships;
-    
+    public BoardField[,] Fields; // 2D-Array für die Spielfelder
 
     public GameBoard(int size)
     {
         Size = size;
         
-        // Generate Ships
+        // Generate Ships randomly
         Ships = new();
+        
+        GenerateFields();
     }
 
     public bool Target(int x, int y)
     {
         throw new NotImplementedException();
+    }
+
+    private void GenerateFields()
+    {
+        //creates a 2d-Array for the gameboardfields
+        Fields = new BoardField[Size, Size];
+        
+        //Initialize each field with a new BoardField-Object
+        for (int i = 0; i < Size; i++)
+        {
+            for (int j = 0; j < Size; j++)
+            {
+                Fields[i, j] = new BoardField();
+            }
+        }
     }
 }
